06e76d2f25db86de8b96a4d0cb938091edc0b890
driven <aldazpaulo@gmail.com>
2022-03-25 23:05:43 -0500

    creando proyecto

---
+++ src/main/java/ec/edu/uce/ProyectoFinalPa2PaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.uce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalPa2PaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalPa2PaApplication.class, args);
+	}
+
+}
